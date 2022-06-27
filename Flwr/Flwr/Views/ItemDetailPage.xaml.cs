6efa141f048f9de0628ad9b44fcbe6aef0902680
Nurzhan Aitbayev <nurzhan@outlook.com>
2022-06-27 17:07:52 +0600

    Add project files.

---
+++ Flwr/Flwr/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Flwr.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Flwr.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Flwr/Flwr/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Flwr.Models;
+using Flwr.ViewModels;
+using Flwr.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Flwr.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
